b3009b06e87f04f4c8d1b9de1f82bb0008a61e7e
nami <almost.nami@gmail.com>
2022-09-23 16:02:38 +0900

    commit 4

--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -41,4 +41,6 @@ public class BoardVO {
     private String writer;
     private Date regdate;
     private Date updateDate;
+
+    private int replyCnt;
 }
--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 
@@ -29,4 +30,6 @@ public interface BoardMapper {
 
     // 전체 개수 구하기
     public int getTotalCount(Criteria cri);
+
+    public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
 }
